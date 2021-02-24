3c8cafa1e0c6ed2619eb70881f827a1584f10042
Angelismar Magallanes <angelis732@gmail.com>
2021-02-24 15:14:17 -0300

    Add role when creating user

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,31 +1,51 @@
 const express = require("express");
 const router = express.Router();
-const { User } = require('../sqlDB')
+const { User, Role } = require('../sqlDB')
 
-
+//Route to create User
 router.post('/' , (req, res, next) => {
-  let { firstName, lastName, email, password, country } = req.body;
+  let { firstName, lastName, email, password, roles } = req.body;
+  
+  roles = roles.map(role => {
+    return  Role.findByPk(role)
+  })
+  
   User.findOne({
     where:{
       email: email
     }
-  }).then(user =>{
-    if(!user){
-      User.create({
-          firstName: firstName,
-          lastName: lastName,
-          email: email,
-          password: password,
-      }).then(newUser => {
-        res.status(201).json(newUser);
-      })
-        .catch(err => {
-          res.status(400).send(err)
-        })
-    }
-  }).catch(err => {
-    next(err)
   })
+    .then(user =>{
+      if(!user){
+        User.create({
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password,
+        }).then(user => {
+          Promise.all(roles).then(roles => {
+            user.addRoles(roles).then(() => {
+              User.findOne({
+                where: {
+                  email: email
+                },
+                attributes: {
+                  include: ['firstName', 'lastName', 'id'],
+                  exclude: ['password']
+                },
+                include:{
+                  model: Role,
+                  as: 'roles',
+                }
+              }).then(user => {
+                res.json(user)
+              })
+            });
+          })
+        })
+        .catch(err => res.status(400).json(err))
+      }
+  }).catch(err => next(err))
 });
 
 module.exports = router;
