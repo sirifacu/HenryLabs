0148dfd85ed9a7d20ea0f48063467d6486084f19
Zaba <Nzabattaro@gmail.com>
2021-03-02 12:06:47 -0300

    Refactor Cohort List / Create Cohort Form
    - Refactoring Cohort Table and add Styles
    - Refactoring Create Cohort and styles
    - Change create cohort to modal type
    - Make both components work with darkmode

--- client/src/components/dashboard/cohort/Cohort.jsx
+++ client/src/components/dashboard/cohort/Cohort.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import CreateCohortForm from './CreateCohortForm';
-import ShowCohorts from './ShowCohorts';
+import ListCohort from './cohortTable/ListCohort'
+import { Grid } from '@material-ui/core';
 
 const Cohort = () => {
 
   return (
     <>
-      <CreateCohortForm/>
-      <ShowCohorts />
+      <Grid container direction='row' spacing={5} justify='center'>
+        <Grid container item xs={6} justify='center'>
+          <CreateCohortForm/>
+        </Grid>
+        <Grid container item xs={12}>
+          <ListCohort/>
+        </Grid>
+      </Grid>
+      
+      
     </>
   );
 };
--- client/src/components/dashboard/cohort/styles/index.js
+++ client/src/components/dashboard/cohort/styles/index.js
@@ -1,7 +1,50 @@
-import { makeStyles } from '@material-ui/core';
+import { fade, lighten, makeStyles } from '@material-ui/core';
 
-export const useStylesCohortForm = makeStyles(theme => ({
-    submitButton: {
-      marginTop: '6%',
+ export const listCohortStyles = makeStyles((theme) => ({
+    root: {
+      width: '100%',
+    },
+    paper: {
+      width: '100%',
+      marginBottom: theme.spacing(2),
+      backgroundColor: theme.palette.grey[400]
+    },
+    table: {
+      minWidth: 750,
+    },
+    visuallyHidden: {
+      border: 0,
+      clip: 'rect(0 0 0 0)',
+      height: 1,
+      margin: -1,
+      overflow: 'hidden',
+      padding: 0,
+      position: 'absolute',
+      top: 20,
+      width: 1,
+    },
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    lectureDetail: {
+        backgroundColor: 'white',
+        maxWidth: '50%',
+        margin: 'auto'
+    },
+    Pagination:{
+      backgroundColor: theme.palette.grey[500]
     }
 }));
+
+export const enhancedTableToolbarStyles = makeStyles((theme) => ({
+  toolbarTable:{
+    backgroundColor: theme.palette.grey[500]
+  },
+  title: {
+      marginTop: theme.spacing(2),
+      flex: '1 1 100%',
+      color: 'black'
+    },
+  }));
