5aebb666e5caa7d2276377e2a9afb81f4fde66e3
Facundo <facundomarilao@gmail.com>
2021-03-03 15:09:38 -0300

    fix bugs invite and register

--- client/src/redux/inviteReducer/actionsInvite.js
+++ client/src/redux/inviteReducer/actionsInvite.js
@@ -1,33 +1,41 @@
 import axios from "axios";
+import Swal from 'sweetalert2'
 import { consoleLog } from '../../services/consoleLog';
 
 export const INVITE_STUDENT= 'INVITE_STUDENT';
 
 export const inviteStudent = (data) => (dispatch) => {
-    const promises = data && data.map((student) => {
-        new Promise((resolve, reject) => {
-            resolve(
-                axios
-                .post(`/users/createUser`, {
-                    firstName: student[0],
-                    lastName: student[1],
-                    email: student[2],
-                    password: student[3]
-                }).then((res) => {
-                    axios.post(`cohorts/${student[4]}/user/${res.data.id}`)
+    if(data[0][0] === ""){
+        Swal.fire('Oops...', 'El csv está vacio', 'error')
+    }else{       
+        const promises = data && data.map((student) => {
+                new Promise((resolve, reject) => {
+                    resolve(
+                        axios
+                        .post(`/users/createUser`, {
+                            firstName: student[0],
+                            lastName: student[1],
+                            email: student[2],
+                            password: student[3]
+                        })
+                        .then((res) => {
+                            axios
+                            .post(`/users/invite`, {
+                                firstName: student[0],
+                                lastName: student[1],
+                                email: student[2],
+                            })
+                        })
+                        .then((res) => {Swal.fire({
+                            position: 'center',
+                            icon: 'success',
+                            title: 'Alumnos invitados correctamente',
+                        })})
+                    )
                 })
-                .then((res) => {
-                    axios
-                    .post(`/users/invite`, {
-                        firstName: student[0],
-                        lastName: student[1],
-                        email: student[2],
-                    })
-                })
-            )
         })
-    })
-    Promise.all(promises)
-    .then(() => dispatch({ type: INVITE_STUDENT, payload: data }))   
-    .catch(err => consoleLog(err));
+        Promise.all(promises)
+        .then(() => dispatch({ type: INVITE_STUDENT, payload: data }))   
+        .catch(err => consoleLog(err));
+    }
 }
